3f39f7595462a321844d340ec663b8a2674a755b
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-09-02 19:58:44 -0400

    Added comments

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -6,9 +6,11 @@ using namespace std;
 // Function to perform Insertion Sort
 void insertionSort(int arr[], int n) {
 
+    // Start from the second element (index 1) because
+    // the first element alone is already "sorted"
     for (int i = 1; i < n; i++) {
-        int key = arr[i];    
-        int j = i - 1; 
+        int key = arr[i];    // The element we want to insert into the sorted portion
+        int j = i - 1;       // Index of the last element in the sorted portion
     
     
     
